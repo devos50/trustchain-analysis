88acf43ccd6a0a5b67c9ff6911464e6b54a91b16
Martijn de Vos <mdmartijndevos@gmail.com>
2020-07-17 09:17:51 +0200

    Graph improvements

--- r/plot_identities_per_day.r
+++ r/plot_identities_per_day.r
@@ -9,10 +9,20 @@ dat$cumsum <- cumsum(dat$new_identities)
 
 p <- ggplot(dat, aes(x=day, y=cumsum)) +
      geom_line() +
-     scale_x_date(date_breaks="1 month") +
+     scale_x_date(date_breaks="1 month", date_labels="%y-%m-%d") +
      theme_bw() +
-     xlab("Date") +
-     ylab("Total identities") +
-     theme(axis.text.x = element_text(angle = 90))
+     xlab("Date (Year-Month-Day)") +
+     ylab("Total TrustChain identities") +
+     theme(axis.text.x = element_text(angle = 90)) +
+     geom_vline(xintercept=as.numeric(as.Date("2020-06-18")), size=0.5, linetype=4) +
+     annotate("text", x=as.Date("2020-07-03"), label="v7.5.0", y=8000, angle=90) +
+     geom_vline(xintercept=as.numeric(as.Date("2020-02-01")), size=0.5, linetype=4) +
+     annotate("text", x=as.Date("2020-02-15"), label="v7.4.0", y=8000, angle=90) +
+     geom_vline(xintercept=as.numeric(as.Date("2019-08-27")), size=0.5, linetype=4) +
+     annotate("text", x=as.Date("2019-09-11"), label="v7.3.0", y=8000, angle=90) +
+     geom_vline(xintercept=as.numeric(as.Date("2019-01-31")), size=0.5, linetype=4) +
+     annotate("text", x=as.Date("2019-02-14"), label="v7.2.0", y=8000, angle=90) +
+     geom_vline(xintercept=as.numeric(as.Date("2018-10-09")), size=0.5, linetype=4) +
+     annotate("text", x=as.Date("2018-10-24"), label="v7.1.0", y=15000, angle=90)
 
-ggsave(filename="identities_per_day.pdf", plot=p, width=10, height=3)
+ggsave(filename="identities_per_day.pdf", plot=p, width=6, height=3)
